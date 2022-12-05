81477d7b8e519dddfbfb66dd59d20959409344aa
Dinesh7a <119056413+Dinesh7a@users.noreply.github.com>
2022-12-05 21:05:39 +0530

    Add files via upload

---
+++ dinu.js
@@ -0,0 +1,11 @@
+document.write()
+alert("average marks")
+var math=35
+var english=40
+var physics=90
+var biology=80
+var chemistry=70
+var python=60
+var a =(math+english+physics+biology+chemistry+python)/6
+
+document.write("average is: ",a)
